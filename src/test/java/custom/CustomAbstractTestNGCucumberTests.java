e31bfccf53ce3939ca0c6dbc154aabfbb1153c33
pnk126 <pnk126@gmail.com>
2025-09-22 02:47:36 -0700

    enabled allure report and fixed classes as per latest cucumber version

--- src/test/java/custom/CustomAbstractTestNGCucumberTests.java
+++ src/test/java/custom/CustomAbstractTestNGCucumberTests.java
@@ -1,50 +1,33 @@
-// package custom;
-// //
-// // Source code recreated from a .class file by IntelliJ IDEA
-// // (powered by Fernflower decompiler)
-// //
+package custom;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-// import io.cucumber.testng.AbstractTestNGCucumberTests;
-// import io.cucumber.testng.CucumberOptions;
-// import io.cucumber.testng.FeatureWrapper;
-// import io.cucumber.testng.PickleWrapper;
-// import org.testng.annotations.AfterClass;
-// import org.testng.annotations.BeforeClass;
-// import org.testng.annotations.DataProvider;
-// import org.testng.annotations.Test;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-// @CucumberOptions(
-//         strict = true,
-//         plugin = {"json:target/cucumber-report-feature-composite.json"}
-// )
-// public class CustomAbstractTestNGCucumberTests extends AbstractTestNGCucumberTests {
-//     @BeforeClass(
-//             alwaysRun = true
-//     )
-//     public void setUpClass() throws Exception {
-//         super.setUpClass();
-//     }
+@CucumberOptions(
+        plugin = {"json:target/cucumber-report-feature-composite.json"}
+)
+public class CustomAbstractTestNGCucumberTests extends AbstractTestNGCucumberTests {
+    @BeforeClass(
+            alwaysRun = true
+    )
+    public void setUpClass(org.testng.ITestContext context) {
+        super.setUpClass(context);
+    }
 
-//     @Test(
-//             groups = {"cucumber"},
-//             description = "Runs Cucumber Scenarios",
-//             dataProvider = "scenarios"
-//     )
-//     public void scenario(PickleWrapper pickleEvent, FeatureWrapper cucumberFeature) throws Throwable {
-//         super.scenario(pickleEvent.getPickleEvent(), cucumberFeature.getCucumberFeature());
-//     }
-
-//     @DataProvider(
-//             parallel = true
-//     )
-//     public Object[][] scenarios() {
-//         return super.scenarios();
-//     }
-
-//     @AfterClass(
-//             alwaysRun = true
-//     )
-//     public void tearDownClass() throws Exception {
-//         super.tearDownClass();
-//     }
-// }
+    @Test(
+            groups = {"cucumber"},
+            description = "Runs Cucumber Scenarios",
+            dataProvider = "scenarios"
+    )
+    public void scenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+        super.runScenario(pickleWrapper, featureWrapper);
+    }
+}
